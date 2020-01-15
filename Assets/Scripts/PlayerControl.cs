ce650dfe44465f003e09c922caca0aa2e015baa6
Kafka Wanna Fly <tarzanchemgio@gmail.com>
2020-01-15 14:21:17 +0700

    Initial scene

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerControl : MonoBehaviour
+{
+    public float m_Speed = 5f;
+    PlayerMotor m_PlayerMotor;
+
+    private void Awake()
+    {
+        m_PlayerMotor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        m_PlayerMotor.m_Velociy = getCurrentVelocity();
+    }
+
+    Vector2 getCurrentVelocity()
+    {
+        float horizontalMov = Input.GetAxisRaw("Horizontal") * m_Speed;
+        float verticalMov = Input.GetAxisRaw("Vertical") * m_Speed;
+        Vector2 velo = new Vector2(horizontalMov, verticalMov);
+
+        return velo;
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMotor : MonoBehaviour
+{
+    public Vector2 m_Velociy { get; set; }
+    public Animator m_Animator;
+    Rigidbody2D m_Rigidbody;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        doAnimation();
+    }
+
+    private void FixedUpdate()
+    {   
+        //Because of using a rigidbody, doMovement should put into FixxedUpdate
+        doMovement();
+    }
+
+    void doMovement()
+    {
+        if(this.transform.localScale.x > 0) //Facing right
+        {
+            if(m_Velociy.x < 0) //Turn left
+            {
+                Vector3 flipedScale = transform.localScale;
+                flipedScale.x *= -1;
+
+                this.transform.localScale = flipedScale;
+            }
+        }
+        else if (this.transform.localScale.x < 0) //Facing left
+        {
+            if (m_Velociy.x > 0) //Turn right
+            {
+                Vector3 flipedScale = transform.localScale;
+                flipedScale.x *= -1;
+
+                this.transform.localScale = flipedScale;
+            }
+        }
+
+        m_Rigidbody.MovePosition(this.m_Rigidbody.position + m_Velociy * Time.fixedDeltaTime);
+    }
+
+    void doAnimation()
+    {
+        if (m_Animator != null)
+        {
+            m_Animator.SetFloat("xSpeed", m_Velociy.magnitude);
+        }
+    }
+}
